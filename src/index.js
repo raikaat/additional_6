4213f07ed249544288f1217128c6a87451bed3d2
raikaat <raikaat@gmail.com>
2017-09-17 14:23:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var str = String(expression);
+    var arr = str.split("*");
+    var arr2 = [];
+    var arr3 = [];  
+	var result = 1;
+    for (var i = 0; i < arr.length; i++) {
+    	if (arr[i].indexOf("!!") === -1) {
+        	arr2.push(parseInt(arr[i]));
+        } else {
+        	arr3.push(parseInt(arr[i]));
+        }
+
 }
+for (var i = 0; i < arr2.length; i++) {
+	result *=fact(arr2[i]);
+    }
+for (var i = 0; i < arr3.length; i++) {
+	result*=doubleFact(arr3[i]);
+}
+return findZeros(result);  
+}
+
+function doubleFact (arg) {
+	if (arg <= 0) 
+	return 1; 
+
+	else return arg * doubleFact(arg-2);
+}
+
+function fact (arg) {
+if (arg === 0) {
+	return 1;
+}
+else return arg*fact(arg-1);
+}
+
+function findZeros(argum) {
+var arg = String(argum);
+ var arr = arg.split("").reverse();
+	var count = 0;
+ for (var i = 0; i < arr.length; i++) {
+	if (arr[i] == "0") {count++}
+	else return count;
+}
+}
+
